83c21f585a1cc1f0f4c5872ddb6128ddbfbdda1c
Jana <jana.moustafa.2024@aiu.edu.eg>
2026-06-20 13:53:05 +0300

    changes commited to app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
-from huggingface_hub import hf_hub_download
+import os
+from huggingface_hub import inferenceClient
+
+
 st.write("""
 # My first app
 Hello *world!*
@@ -10,6 +13,15 @@ name=st.text_input("Enter your name", "Type Here ...")
 if name:
     st.write('Hello, ', name, '!')
     
+input_text=st.text_input("Enter your text", "Type Here ...")
 client=inferenceClient(
-    provider="auto",api_key=os.environ.get["HUGGINGFACEHUB_API_TOKEN"]
+    provider="auto",api_key=os.environ.get("HUGGINGFACEHUB_API_TOKEN")
 )
+
+if input_text:
+    output=client.text_classification(
+        model="distilbert-base-uncased-finetuned-sst-2-english",
+        inputs=input_text
+        
+    )   
+    st.write(output)
