4427a899d174d68d1119965127fc4e8ff0a50eb2
Jana <jana.moustafa.2024@aiu.edu.eg>
2026-06-20 18:48:07 +0300

    fix text classification and load_dotenv

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 import streamlit as st
-import pandas as pd
+from dotenv import load_dotenv
 import os
-from huggingface_hub import inferenceClient
+from huggingface_hub import InferenceClient
 
+load_dotenv(dotenv_path=Path(__file__).parent / ".env")
 
-st.write("""
-# My first app
-Hello *world!*
-""")
 st.title('My first app') 
 name=st.text_input("Enter your name", "Type Here ...")
 if name:
     st.write('Hello, ', name, '!')
     
 input_text=st.text_input("Enter your text", "Type Here ...")
-client=inferenceClient(
+client=InferenceClient(
     provider="auto",api_key=os.environ.get("HUGGINGFACEHUB_API_TOKEN")
 )
 
 if input_text:
-    output=client.text_classification(
-        model="distilbert-base-uncased-finetuned-sst-2-english",
-        inputs=input_text
-        
-    )   
-    st.write(output)
+   output = client.text_classification(
+    model="distilbert-base-uncased-finetuned-sst-2-english",
+    text=input_text
+)
+st.write(output)
+print(os.environ.get("HUGGINGFACEHUB_API_TOKEN"))
